d99cf98255033e9dfbc1f8013a9e503985926974
ritiksingh11661 <delhi8260@gmail.com>
2023-05-12 15:29:54 +0530

    tailwind css added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Revision Project</h1>
+      <h1 className='text-3xl font-bold underline text-red-200'>Revision Project</h1>
     </div>
   );
 }
